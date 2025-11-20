e6c06c1230f2a137b147a825cb3979be334ec1b9
Joepeijkens <joep@eijkens.com>
2025-11-20 16:07:53 +0100

    Add 'hello' to Discrete_event_simulation.R

--- Discrete_event_simulation.R
+++ Discrete_event_simulation.R
@@ -4,3 +4,4 @@ library(simmer)
 url <- "https://raw.githubusercontent.com/Joepeijkens/Computational-assignment-Maastricht-University-team-11-2025-2026/0cffd7a386b5c0d1e46f01ed35e4fd8cf61d82bb/ScanRecords.csv"
 
 data <- read.csv(url)
+ hello
